78842a0d689692e3a48ebb35da5348029f904ea9
jumin <kjm541113@gmail.com>
2024-04-28 23:24:49 +0900

    스크롤바 추가 및 스타일링

--- app/_components/CardMain.js
+++ app/_components/CardMain.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CardMainStyle from "./CardMainStyle.css";
 
 const CardMain = ({ info }) => {
   return (
@@ -6,14 +7,14 @@ const CardMain = ({ info }) => {
       {info.isCityExist && (
         <>
           <div className="text-lg font-bold mb-2">예보</div>
-          <div className="flex gap-6 overflow-x-auto whitespace-nowrap scrollbar-hide bg-blue-500">
+          <div className="flex gap-6 overflow-x-auto whitespace-nowrap bg-blue-300 rounded-md p-4 pb-3 scrollbar">
             {info.forecastInfo?.map((forecast) => {
               const dateString = forecast.dt;
               const date = new Date(dateString * 1000);
               const day = date.toLocaleDateString("ko-KR", { day: "numeric" });
               const hour = date.toLocaleTimeString("ko-KR", { hour: "2-digit", hour12: true });
               return (
-                <div key={forecast.dt} className="">
+                <div key={forecast.dt} className="text-center">
                   {/* <div>시간 : {forecast.dt_txt}</div> */}
                   <div>시간 : {`${day} ${hour}`}</div>
                   <div>기온 : {Math.round(forecast.main.temp)}°C</div>
@@ -21,7 +22,6 @@ const CardMain = ({ info }) => {
                   <div>습도 : {Math.round(forecast.main.humidity)}%</div>
                   <div>날씨 : {forecast.weather[0].main}</div>
                   <div>풍향 : {forecast.wind.deg}°</div>
-                  <br />
                 </div>
               );
             })}
