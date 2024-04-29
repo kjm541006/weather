8f5d236e2921a4ee595191c11596178bae282507
kjm541006 <kjm541113@gmail.com>
2024-04-29 15:56:18 +0900

    스크롤바 사파리 브라우저 버그 테스트

--- app/_components/CardMain.js
+++ app/_components/CardMain.js
@@ -8,8 +8,8 @@ const CardMain = ({ info }) => {
     <>
       {info.isCityExist && (
         <>
-          <div className="text-lg font-bold mb-2">예보 ({info.isMyLoc ? "" : info.city + " "}현지 시간 기준)</div>
-          <div className="flex gap-6 overflow-x-auto whitespace-nowrap scrollbar bg-blue-300 rounded-md">
+          <div className="text-md font-bold mb-2">예보 ({info.isMyLoc ? "" : info.city + " "}현지 시간 기준)</div>
+          <div className="flex gap-1 overflow-x-auto whitespace-nowrap scrollbar bg-blue-300 rounded-md">
             {info.forecastInfo.list.map((forecast) => {
               // 밀리초 단위의 Unix 타임스탬프를 Date 객체로 변환
               const dateObject = new Date((forecast.dt + timeZone) * 1000);
@@ -28,11 +28,16 @@ const CardMain = ({ info }) => {
                 <div key={forecast.dt} className="flex flex-col items-center gap-3 mx-8 my-4">
                   {/* <div>시간 : {forecast.dt_txt}</div> */}
                   <div className="text-white font-semibold text-md">{`${weekday}일(${day}) ${hour}시`}</div>
-                  <div className="text-sm">기온 : {Math.round(forecast.main.temp)}°C</div>
-                  <div className="text-sm">체감 : {Math.round(forecast.main.feels_like)}°C</div>
-                  <div className="text-sm">습도 : {Math.round(forecast.main.humidity)}%</div>
-                  <div className="text-sm">날씨 : {forecast.weather[0].main}</div>
-                  <div className="text-sm">풍향 : {forecast.wind.deg}°</div>
+                  {forecast.weather[0].main === "Clouds" && <img src="/images/clouds.png" className="w-16 h-16" alt="구름" />}
+                  {forecast.weather[0].main === "Snow" && <img src="/images/snow.png" className="w-16 h-16" alt="눈" />}
+                  {forecast.weather[0].main === "Clear" && <img src="/images/clear.png" className="w-16 h-16" alt="맑음" />}
+                  {forecast.weather[0].main === "Rain" && <img src="/images/rain.png" className="w-16 h-16" alt="비" />}
+                  {forecast.weather[0].main === "Drizzle" && <img src="/images/drizzle.png" className="w-16 h-16" alt="이슬비" />}
+                  <div className="text-md text-white">{Math.round(forecast.main.temp)}°C</div>
+                  {/* <div className="text-sm">체감 : {Math.round(forecast.main.feels_like)}°C</div> */}
+                  {/* <div className="text-sm">습도 : {Math.round(forecast.main.humidity)}%</div> */}
+                  {/* <div className="text-sm">날씨 : {forecast.weather[0].main}</div> */}
+                  {/* <div className="text-sm">풍향 : {forecast.wind.deg}°</div> */}
                 </div>
               );
             })}
